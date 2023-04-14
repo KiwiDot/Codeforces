0a11a1e40c821cd6b1b789df434da7f301dfb1f9
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2023-04-14 10:43:09 +0900

    -
    2023-04-14

---
+++ 1816/A. Ian Visits Mary/1816A.py
@@ -0,0 +1,13 @@
+# 코드포스 1816A Ian Visits Mary
+import sys
+put = sys.stdin.readline
+
+t = int(put())
+
+while t:
+    t -= 1
+    a, b = map(int, put().split())
+
+    print(2)
+    print(a - 1, 1)
+    print(a, b)
